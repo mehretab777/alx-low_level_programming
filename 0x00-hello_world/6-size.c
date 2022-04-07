b5532ebd36d6ff3a8276317d4adf9da9d7157880
mehretab <mehretab7@yahoo.com>
2022-04-07 14:22:20 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ printf("size of a char: %d byte(s)\n", size of (char));
 printf("size of an int: %d byte(s)\n", size of (int));
 printf("size of a long int: %d byte(s)\n", size of (long int));
 printf("size of a long long int: %d byte(s)\n", size of (long long int));
-printf("size of a float in: %d byte(s)\n", size of (float));
+printf("size of a float: %d byte(s)\n", size of (float));
 return(0);
 }
 
