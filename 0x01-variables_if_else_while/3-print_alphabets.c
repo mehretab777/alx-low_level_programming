01e5a39f56675c230162cc578a1ff8bd2f9ee79c
mehretab <mehretab7@yahoo.com>
2022-04-08 14:16:41 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,7 +16,10 @@ for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 putchar(lower_case);
 }
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)                         {                                                                               putchar(upper_case);                                                            }
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)                                                                                                                            
+{                                                                                                                                                                                  
+putchar(upper_case);                                                                                                                                                               
+}
 
 putchar ('\n');
 
