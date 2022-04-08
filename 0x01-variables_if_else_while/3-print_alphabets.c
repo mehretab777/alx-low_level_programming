7877640b67306cf7d75b3d9d57d0dd8300b157f1
mehretab <mehretab7@yahoo.com>
2022-04-08 13:16:01 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success/correct)
+ */
+int main(void)
+
+{
+char lower-case;
+char upper-case;
+
+for (upper-case = 'A'; upper-case <= 'Z'; upper-case++)
+{
+putchar(upper-case);
+}
+
+for (lower-case = 'a'; lower-case <= 'z'; lower-case++)                                                                                                                            
+{                                                                                                                                                                                  
+putchar(lower-case);                                                                                                                                                               
+}
+
+putchar('\n');
+
+return (0);
+
+}
