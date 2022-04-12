5ca8cc65681b4773d1d845773207270b73270c09
mehretab <mehretab7@yahoo.com>
2022-04-12 15:29:04 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,13 +13,13 @@ unsigned long fib1 = 0, fib2 = 1, sum;
 for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
-printf("%d\n", sum);
+printf("%lu", sum);
 
 fib1 = fib2;
 fib2 = sum;
 
 if (count == 49)
-printf("%d\n");
+printf("\n");
 else
 printf(',');
 }
