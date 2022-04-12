240ce9fa59c602c4001d57d094c3a6b98b7e760a
mehretab <mehretab7@yahoo.com>
2022-04-12 15:22:59 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0;
+ */
+int main(void)
+{
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+
+for (count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%d\n", sum);
+
+fib1 = fib2;
+fib2 = sum;
+
+if (count == 49)
+printf("%d\n");
+else
+printf(',');
+}
+
+return (0);
+}
