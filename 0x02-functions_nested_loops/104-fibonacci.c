2ba2bc2aee3063972fdd4dbe141bc91394f7b39e
mehretab <mehretab7@yahoo.com>
2022-04-13 15:55:33 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
+  * main - print a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
   *
   * Return: Always 0.
   */
