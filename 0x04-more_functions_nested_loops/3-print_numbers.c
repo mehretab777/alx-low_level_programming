517e4fcbc1b9f91d5b40cc6c7664d677d2cabae5
mehretab <mehretab7@yahoo.com>
2022-04-14 13:31:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints the numbers, from 0 to 9
+  *@n: print number
+  * Return: Always 0.
+  */
+
+void print_numbers(void);
+{
+
+if (n >= 0 && n <= 9)
+{
+_putchar ("%d", n);
+}
+
+_putchar ('\n');
+
+}
