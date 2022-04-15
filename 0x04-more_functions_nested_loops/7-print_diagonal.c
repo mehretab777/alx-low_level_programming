e5512054e750b00ec1addc96ef96a1946d365fb2
mehretab <mehretab7@yahoo.com>
2022-04-15 05:13:48 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: limit
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n>0)
+{
+for (i = 0; i < n; i++)
+{
+for (i = 0; i < 1; i++)
+{
+_putchar (' ');
+}
+_putchar ('\\');
+_putchar ('\n');
+}
+}
+else
+{
+_putchar ('\n');
+}
+}
+
