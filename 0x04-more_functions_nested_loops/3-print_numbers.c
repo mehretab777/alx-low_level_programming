ad757945686bb944831363f6bb5fdd0e02d6d827
mehretab <mehretab7@yahoo.com>
2022-04-15 04:45:23 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
   * print_numbers - prints numbers
+  *
   * Return: Always 0 (success)
   */
 
@@ -11,8 +12,8 @@ int i;
 
 i = 0;
 for (i = 0; i < 10; i++)
-{
+
 _putchar (i + '0');
-}
+
 _putchar ('\n');
 }
