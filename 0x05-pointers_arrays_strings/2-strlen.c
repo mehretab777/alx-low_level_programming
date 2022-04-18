bb3616506e28334c74e55fe9a657b4f36f239fa4
mehretab <mehretab7@yahoo.com>
2022-04-18 12:29:04 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  * _strlen: length of a string
  * @s: pointer to an int that will be changed/updated
  *
- * Return: void that maeans our answer is correct 
+ * Return: void that means our answer is correct 
  */
 
 int _strlen(char *s)
