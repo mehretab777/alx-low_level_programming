0801590162adb51722e3b068a8d336da97941c94
mehretab <mehretab7@yahoo.com>
2022-04-18 11:33:53 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update integer to 98
+ * @n: pointer to an int that will be updated
+ *
+ * Return: void that means is correct
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
