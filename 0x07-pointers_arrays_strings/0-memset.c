642f6043f885254a15685fe6fbbcb630557bcc0a
mehretab <mehretab7@yahoo.com>
2022-04-26 09:12:58 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,10 @@ include "main.h"
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-i = 0;
+       unsigned int i;
 
-while (i < n) 
-{
-s[i] = b;
-i++;
-}
-return (s);
+     for (i = 0; i < n; i++)
+	     *(s + i) =  b;
+     return (s);
 }
 
