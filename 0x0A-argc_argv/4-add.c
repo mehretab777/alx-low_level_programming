e20b01cfeaea1be34a92efbe3d0fdcde24f6a1c2
mehretab <mehretab7@yahoo.com>
2022-04-29 11:54:10 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,6 @@ unsigned int count;
 
 count = 0;
 while (count < strlen(str)) /*count string*/
-
 {
 if (!isdigit(str[count])) /*check if str there are digit*/
 {
@@ -38,7 +37,6 @@ return (1);
  */
 
 int main(int argc, char *argv[])
-
 {
 /*Declaring variables*/
 int count;
@@ -49,7 +47,6 @@ count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
 if(check_num(argv[count]))
-
 {
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
